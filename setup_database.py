5ea08ae6edfc0aaee4effe757771d6c91d63d5f1
Dumisani Mbonani <dumimboanani@gmail.com>
2023-10-21 12:16:13 +0200

    cleaning the project repo

--- setup_database.py
+++
@@ -1,8 +0,0 @@
-from base.base import Database
-import os
-
-# Setup the database
-database = Database("database/psw_manager.db")
-
-# Create vault table with password
-database.create_table("vault", "id INTEGER PRIMARY KEY AUTOINCREMENT, password TEXT")
